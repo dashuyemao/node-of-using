06b9e87a8f728c70f934fd152300ca612a86f4d7
dashuyemao <2234764648@qq.com>
2017-04-07 10:33:15 +0800

    class

---
+++ serveer.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get("/",function(req,res){
+	res.send('hello node')
+})
+
+app.get("/getstudent",function(req,res){
+	res.send('小明')
+})
+
+app.listen(8080,function(){
+	console.log('server is running!')
+})
